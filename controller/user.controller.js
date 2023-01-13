fc6c51d4bb0166443a9a2b5ccc353701d7f5f708
Angelina <ak1@digintel.ru>
2023-01-13 19:02:39 +0300

    test

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,10 +2,10 @@ const db = require('../db');
 
 class UserController {
     async createUser(req, res) {
-        const {name, surname} = req.body
-        const newPerson = await db.query(`INSERT INTO person (name, surname) values ($1, $2) RETURNING *`, [name, surname])
-        console.log(name, surname)
-        res.json(newPerson.rows[0])
+        const query = req.body
+        await db.query(query)
+        console.log(query)
+        res.json(query)
     }
     async getUsers(req, res) {
         const users = await db.query('SELECT * FROM person')
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const http = require('http');
 const express = require('express');
 const userRouter = require('./routes/user.routes');
-const postRouter = require('./routes/post.routes');
 const port = process.env.port || 5000
 
 const app = express()
@@ -9,7 +8,6 @@ const server = http.createServer(app)
 
 app.use(express.json())
 app.use('/api', userRouter)
-app.use('/api', postRouter)
 
 app.listen(port, () => {
     console.log(`server started on port ${port}`)
